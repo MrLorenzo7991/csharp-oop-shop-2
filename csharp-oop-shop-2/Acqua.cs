382299d2f375dad8ba0aa074650f88c1ee377f56
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-12 15:18:48 +0200

    Completati setter e getter acqua e metodo bevi

--- csharp-oop-shop-2/Acqua.cs
+++ csharp-oop-shop-2/Acqua.cs
@@ -14,6 +14,48 @@ namespace csharp_oop_shop_2
         public Acqua(string nome, double litri ,double prezzo, int iva) : base(nome, prezzo, iva)
         {
             this.litri = litri;
+            ph = 7;
+            sorgente = "Fonte Guizza";
+
+        }
+
+        //getter acqua
+        public double GetLitri()
+        {
+            return litri;
+        }
+        public double GetPh()
+        {
+            return ph;
+        }
+        public string GetSorgente()
+        {
+            return sorgente;
+        }
+        //Setter acqua
+        public void SetLitri(double litri)
+        {
+            this.litri = litri;
+        }
+
+
+        //Metodo Bevi
+        public void Bevi(double litriDaBere)
+        {
+            if (litriDaBere < 0)
+            {
+                Console.WriteLine("Non puo bere litri negativi");
+            }
+            else if (litriDaBere > litri)
+            {
+                Console.WriteLine("Vuoi bere più litri di quanti ce ne siano, hai bevuto tutta l'acqua");
+                litri = 0;
+            }
+            else
+            {
+                litri -= litriDaBere;
+                Console.WriteLine("Hai bevuto " + litriDaBere + " litri, ti rimangono " + litri + " litri");
+            }
         }
     }
 }
--- csharp-oop-shop-2/Program.cs
+++ csharp-oop-shop-2/Program.cs
@@ -3,3 +3,9 @@
 Prodotto fagioli = new Prodotto("Fagioli", "scatolo grande rosso", 2.30, 22);
 
 fagioli.StampaProdotto();
+
+Acqua acquaGuizza = new Acqua("Acqua Guizza", 2, 0.40, 4);
+acquaGuizza.StampaProdotto();
+acquaGuizza.Bevi(1.3);
+acquaGuizza.Bevi(-2);
+acquaGuizza.Bevi(0.7);
